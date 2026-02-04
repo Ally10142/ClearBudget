2aa0725e428a7df04df84d781c3632de952e42c9
Patrick <Patrickrice911@gmail.com>
2026-02-03 20:04:00 -0600

    Initial Commit

---
+++ ClearBudget/ClearBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearBudgetApp.swift
+//  ClearBudget
+//
+//  Created by Patrick on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ClearBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
